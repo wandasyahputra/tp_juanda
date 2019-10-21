85620923377f539ee3fd4631f6e32c01df640992
Wanda Syahputra <putra@qerja.com>
2019-10-21 07:08:06 +0700

    promo

--- index.php
+++ index.php
@@ -153,7 +153,7 @@
 					</div>
                 </div>
             </div>
-            <div id="highclassfacilities" class="page">
+            <div id="highclassfacilities" class="page container">
 				<div class="title">HIGH CLASS FACILITIES</div>
 				<img class="w-800" src="assets/img/11 water park.jpg">
 				<div class="group">
@@ -185,8 +185,114 @@
 					</p>
 				</div>
 			</div>
+			<div id="typicalfloor" class="page">
+				<div class="container">
+					<div class="col-md-12">
+						<div class="sub-title">Master Plan</div>
+						<img class="w-800" src="assets/img/17 masterplan-juanda.jpg">
+					</div>
+					<div class="col-md-12">
+						<div class="title">Floor Plan / GF</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/18 GF_1.jpg">
+							<div class="sub-title">JADE TOWER</div>
+						</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/18 GF_2.jpg">
+							<div class="sub-title">SAPPHIRE TOWER</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="title">Floor Plan / F1</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/19 F1_1.jpg">
+							<div class="sub-title">JADE TOWER</div>
+						</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/19 F1_2.jpg">
+							<div class="sub-title">SAPPHIRE TOWER</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="title">Typical Floor Plan / TU</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/20 TU_1.jpg">
+							<div class="sub-title">JADE TOWER</div>
+						</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/20 TU_2.jpg">
+							<div class="sub-title">SAPPHIRE TOWER</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="title">Typical Floor</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/21 BR2.jpg">
+						</div>
+						<div class="col-md-6 dualcolumn-container">
+							<img class="w-800" src="assets/img/22 BR2corner.jpg">
+						</div>
+						<img class="w-800" src="assets/img/23 BR3.jpg">
+					</div>
+					<div class="col-md-12 container">
+						<div class="title">Emerald</div>
+						<img class="w-800" src="assets/img/24 page 37.jpg">
+						<img class="w-800" src="assets/img/25 page 38.jpg">
+						<img class="w-800" src="assets/img/26 page39.jpg">
+						<img class="w-800" src="assets/img/27 page40.jpg">
+						<img class="w-800" src="assets/img/28 page41.jpg">
+						<img class="w-800" src="assets/img/29 page42.jpg">
+					</div>
+				</div>
+			</div>
+			<div id="unitprice">
+				<div class="container page">
+					<h3 class="title">UNIT PRICE</h3>
+					<div class="col-md-3 col-md-offset-4">
+						<span class="bold">Starting Rp &nbsp;&nbsp; Million /sqm*</span><br>
+						<br>
+						<span class="bold">Unit Size</span><br>
+						<br>
+						<span class="bold"></span>: 60,29 sqm; 69,39 sqm<br>
+						<br>
+						<span class="bold"></span>: 91,40 sqm; 91, 69 sqm<br>
+						<br>
+						for price details, <span class="bold"></span>please contact us (<a class="no-decoration" href="https://api.whatsapp.com/send?phone=6287878550000" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="12px" height="12px" viewBox="0 0 90 90" style="fill:black;" xml:space="preserve"><g>	<path id="WhatsApp" d="M90,43.841c0,24.213-19.779,43.841-44.182,43.841c-7.747,0-15.025-1.98-21.357-5.455L0,90l7.975-23.522   c-4.023-6.606-6.34-14.354-6.34-22.637C1.635,19.628,21.416,0,45.818,0C70.223,0,90,19.628,90,43.841z M45.818,6.982   c-20.484,0-37.146,16.535-37.146,36.859c0,8.065,2.629,15.534,7.076,21.61L11.107,79.14l14.275-4.537   c5.865,3.851,12.891,6.097,20.437,6.097c20.481,0,37.146-16.533,37.146-36.857S66.301,6.982,45.818,6.982z M68.129,53.938   c-0.273-0.447-0.994-0.717-2.076-1.254c-1.084-0.537-6.41-3.138-7.4-3.495c-0.993-0.358-1.717-0.538-2.438,0.537   c-0.721,1.076-2.797,3.495-3.43,4.212c-0.632,0.719-1.263,0.809-2.347,0.271c-1.082-0.537-4.571-1.673-8.708-5.333   c-3.219-2.848-5.393-6.364-6.025-7.441c-0.631-1.075-0.066-1.656,0.475-2.191c0.488-0.482,1.084-1.255,1.625-1.882   c0.543-0.628,0.723-1.075,1.082-1.793c0.363-0.717,0.182-1.344-0.09-1.883c-0.27-0.537-2.438-5.825-3.34-7.977   c-0.902-2.15-1.803-1.792-2.436-1.792c-0.631,0-1.354-0.09-2.076-0.09c-0.722,0-1.896,0.269-2.889,1.344   c-0.992,1.076-3.789,3.676-3.789,8.963c0,5.288,3.879,10.397,4.422,11.113c0.541,0.716,7.49,11.92,18.5,16.223   C58.2,65.771,58.2,64.336,60.186,64.156c1.984-0.179,6.406-2.599,7.312-5.107C68.398,56.537,68.398,54.386,68.129,53.938z"/></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg></a>)</span><br>
+						<br>
+						<span class="bold">Get Promo This Month</span><br>
+						<span class="italicunderline">Easy Payment Methods</span><br>
+						<br>
+						<span class="bold">Term of Payments</span><br>
+						<span class="bold">Hard Cash : Cash1 (discount 6%),</span><br>
+						<span class="bold">Installment</span><br>
+						<span class="bold">x12, x18, x24 DP50% Discount 3% </span><br>
+						<span class="bold">x12, x18, x24 DP30% Discount 1,5% </span><br>
+						<br>
+						<br>
+						<br>
+						<span class="">Installment : 24x</span><br>
+						<span class="bold">KPA :</span><br>
+						<span class="">1 BR = Rp. 25 Million (x9)</span><br>
+						<span class="">2 BR = Rp. 25 Million (x11)</span><br>
+						<span class="">3 BR = Rp. 25 Million (x17)</span><br>
+						<span class="">Remaining Balance (KPA)</span><br>
+						<br>
+						<br>
+						<span class="italic">(Updated : &nbsp; 2019)</span><br>
+					</div>
+				</div>
+			</div>
+			<div id="promo">
+				<div class="container page">
+					<h3 class="title">PROMO</h3>
+					<a href="assets/img/30 Promo.jpg" data-toggle="lightbox" data-gallery="promo-gallery" class="w-800">
+						<img src="assets/img/30 Promo.jpg" class="w-800">
+					</a>
+				</div>
+			</div>
+
 		<script>			
-			$('#navbars').scrollspy({offset:"-50",animate: true,duration: 750})
+			$('#navbars').scrollspy({offset:"-73",animate: true,duration: 750})
 			$(document).on('click', '[data-toggle="lightbox"]', function(event) {
                 event.preventDefault();
                 $(this).ekkoLightbox();
